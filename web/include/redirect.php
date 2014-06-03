7ad2031772c931540865760c7bcae03d25903f31
mike.charles <mike.charles@noaa.gov>
2014-06-03 15:57:08 +0000

    Added a redirect page to explain to users the requirements for the VWT with help setting them up. See #1444

---
+++ web/include/redirect.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<style>
+		#container
+		{
+			width:800px;
+			margin:50px auto;
+			text-align:left;
+			padding:15px;
+			border:1px dashed #333;
+			background-color:#eee;
+		}
+	</style>
+</head>
+<body>
+<div id="container">
+	<p>
+	You will soon be directed to CPC's Verification Web Tool (VWT). A few things first:
+	</p>
+	<ol>
+		<li>
+			The VWT has currently only been fully tested in <b>Firefox</b> on Mac, Windows, and Linux. We cannot guarantee that the tool will work in any other browser. <a href="http://www.mozilla.org/en-US/firefox/new/" target="_blank">Click here</a> to download Firefox if you don't already have it.
+		</li>
+		<li>
+			You'll need the <b>Java plugin</b> in order for the tool to load properly:
+			<ul>
+				<li><a href=www.java.com/en/download/installed.jsp" target="_blank">Click here</a> to see whether you have Java installed.</li>
+				<li>If not, <a href=www.java.com/en/download/index.jsp" target="_blank">Click here</a> to download and install the Java plugin.</li>
+				<li>Note that after you install the Java plugin, you will need to restart your browser in order for the plugin to take effect.</li>
+			</ul>
+		</li>
+		<li>
+			When you first access the VWT, 1 or 2 Java-related windows will popup:
+			<ul>
+				<li>For the 1st window, check the box and click "Run".</li>
+				<li>If there's a 2nd window, click "Allow" or "Don't Block". Note that sometimes the popup window is hidden behind the browser.</li>
+			</ul>
+		</li>
+	</ol>
+	<p>
+	If you find any issues when using the VWT, please provide feedback by clicking the "Feedback" button on the bottom right of the page.
+	</p>
+	<input type="submit" value="Access VWT" onclick="window.location='http://www.vwt.ncep.noaa.gov';" />
+</div>
+</body
+</html>
