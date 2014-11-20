fb17456ec3b598bffefd8fd09303392552ab7807
mike.charles <mike.charles@noaa.gov>
2014-11-20 20:02:57 +0000

    Fixed a mistake where the monthly revised wasn't being called correctly

--- scripts/init_db_test_findMissing_batch.sh
+++ scripts/init_db_test_findMissing_batch.sh
@@ -11,7 +11,7 @@ if [[ $fcst_type == 'ERF' ]] ; then
 	./scripts/db_test_findMissing_batch.pl -f ERF -loglevel error -email > $VERIF_HOME/logs/db_test_findMissing_ERF.log 2>&1
 elif [[ $fcst_type == 'monthly' ]] ; then
 	./scripts/db_test_findMissing_batch.pl -f monthly -loglevel error -email > $VERIF_HOME/logs/db_test_findMissing_monthly.log 2>&1
-elif [[ $fcst_type == 'revised_monthly' ]] ; then
+elif [[ $fcst_type == 'monthly_revised' ]] ; then
 	./scripts/db_test_findMissing_batch.pl -f monthly_revised -loglevel error -email > $VERIF_HOME/logs/db_test_findMissing_monthly_revised.log 2>&1
 elif [[ $fcst_type == 'seasonal' ]] ; then
 	./scripts/db_test_findMissing_batch.pl -f seasonal -loglevel error -email > $VERIF_HOME/logs/db_test_findMissing_seasonal.log 2>&1
